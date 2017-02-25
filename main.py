ce8d5ccc3e1391d64c9fcd8ae175db5d05c5d2f8
YangZefan <zefanyang@icloud.com>
2017-02-25 23:29:39 +0800

    i

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import xlrd
 import xlwt
-
+a=[[0]*5]*1000;
 workbook = xlrd.open_workbook(u'2016奖学金打分汇总表.xlsx')
 print(workbook.sheet_names())
 
@@ -11,7 +11,14 @@ print(table.nrows, table.ncols)
 
 # 这张表从第3行开始，29行结束
 # 相当于for(i=2;i<29;i++)
-for i in range(2, 29):
+for i in range(2, 28):
     # 第1列为姓名
-    print(table.cell(i, 0).value)
+    a[i][1]=table.cell(i, 0).value
+    a[i][2]=table.cell(i, 8).value
+
+
+#workbook1 = xlwt.open_workbook(u'2016奖学金打分汇总表.xlsx')
+#table1 = workbook1.sheet_by_name(u'综合优秀')
+#for i in range(2,29):
+#    table.write(i,15,a[i][2])
 
